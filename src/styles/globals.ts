2268df31bccb5c09e235c60ed4447bb0db9b6268
Allan Carvalho <allan.carvalho@dynatrace.com>
2024-07-13 17:52:11 +0200

    feat: added linaria and global styles

---
+++ src/styles/globals.ts
@@ -0,0 +1,22 @@
+import { css } from '@linaria/core';
+
+export const globals = css`
+  :global() {
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    html,
+    body,
+    #root {
+      width: 100%;
+      height: 100%;
+    }
+
+    button {
+      cursor: pointer;
+    }
+  }
+`;
