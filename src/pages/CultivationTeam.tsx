545720fc118b061f786501dfd6c4c2912c66bab9
Allan Carvalho <allan.carvalho@dynatrace.com>
2024-07-15 22:01:23 +0200

    feat: added cultivation name to the cultivation page title

--- src/pages/CultivationTeam.tsx
+++ src/pages/CultivationTeam.tsx
@@ -1,15 +1,21 @@
 import type { FC } from 'react';
+import { useParams } from '@tanstack/react-router';
 
 import { CultivationUsersTable } from '../components/UsersTable/UsersTable';
-
 import { Page } from '../core/components/Page/Page';
+import { useGetCultivations } from '../hooks/use-get-cultivations';
 
 // 94eb21dd-d0d2-4e30-b3b8-51be6c6fa699
 export const CultivationTeam: FC = () => {
+  const { cultivationId } = useParams({ strict: false });
+  const { data } = useGetCultivations();
+
+  const cultivation = data?.find((c) => c.id === cultivationId);
+
   return (
     <Page>
       <Page.Header>
-        <h1>Cultivation Team</h1>
+        <h1>Cultivation Team{cultivation && ` - ${cultivation.name}`}</h1>
       </Page.Header>
       <Page.Body>
         <CultivationUsersTable />
