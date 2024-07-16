abda0007955b5c2774d038efa231e11fb4442167
Allan Carvalho <allan.carvalho@dynatrace.com>
2024-07-16 17:26:19 +0200

    chore: clean up

--- src/core/utils/get-random-value.ts
+++
@@ -1,8 +0,0 @@
-export const getRandomValue = <T>(arrayOfValues: T[]): T => {
-  const min = 0;
-  const max = arrayOfValues.length - 1;
-
-  const index = Math.floor(Math.random() * (max - min + 1) + min);
-
-  return arrayOfValues[index];
-};
